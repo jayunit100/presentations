87fe54b1635949fe1b921a509d1e1059f66ef2f0
jay vyas <jayunit100.github@gmail.com>
2020-03-05 14:06:22 -0500

    Create test.sh

---
+++ netpol-sig-network-ongoing/test.sh
@@ -0,0 +1,30 @@
+kubectl create clusterrolebinding netpol --clusterrole=cluster-admin --serviceaccount=kube-system:netpol
+kubectl create sa netpol -n kube-system
+
+export KUBECONFIG=`kind get kubeconfig-path --name=calico-test` 
+
+######################## 
+
+kubectl create clusterrolebinding netpol --clusterrole=cluster-admin --serviceaccount=kube-system:netpol
+kubectl create sa netpol -n kube-system
+
+
+#######################
+
+kubectl delete job netpol -n kube-system
+kubectl delete ns x
+kubectl delete ns y
+kubectl delete ns z
+
+sleep 2
+
+#######################
+
+kubectl create -f https://raw.githubusercontent.com/vmware-tanzu/antrea/master/hack/netpol/install-latest.yml -n kube-system 
+
+until kubectl logs -f `kubectl get pods -n kube-system | grep net | cut -d' ' -f 1` -n kube-system ; do 
+	echo "trying again in 3 seconds..."
+	sleep 3
+done
+
+
